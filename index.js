7092c27a56f8647980bd1367018a40e2083b1a9e
Seano <sean01234@yahoo.com>
2023-04-15 03:05:45 -0400

    fetch

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
+
+// To pass the tests, don't forget to return your fetch!
+
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((resp) => renderBooks(resp))
+
+  //.then((resp) => console.log(resp))
+  //.then((resp) => resp.json())
+
+
+
+  //.then((data) => console.log(data[0]))
+  //.then((data) => renderBooks(data))
+};
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
+  const main=document.querySelector('main');
   books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    const h2=document.createElement('h2');
+    h2.innerHTML=book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+
+
+
+document.addEventListener('DOMContentLoaded',function() {
+  //fetchBooks()
+  return fetchBooks()
+  //renderBooks(fetchBooks)
+}
+)
